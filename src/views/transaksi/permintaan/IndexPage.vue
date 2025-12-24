99929a46fb886343e1b213b3761b3aa19e16bafd
pigartpigaro <vichoirul@gmail.com>
2025-12-24 23:26:55 +0700

    v

--- src/views/transaksi/permintaan/IndexPage.vue
+++ src/views/transaksi/permintaan/IndexPage.vue
@@ -18,28 +18,90 @@
 </template>
 
 <script setup>
-import { computed, onMounted } from 'vue'
+import { computed, nextTick, onMounted } from 'vue'
 import { defineAsyncComponent } from 'vue'
 import { useMutasiStore } from '@/stores/template/register'
 import { useRoute } from 'vue-router'
+import { useAppStore } from '@/stores/app'
+import { ref } from 'vue'
+import { api } from '@/services/api'
+import { getYearEndDate, getYearStartDate } from '@/utils/dateHelper'
 const BaseTransaksi = defineAsyncComponent(() => import('@/components/templates/BaseTransaksi.vue'))
 const FormPage = defineAsyncComponent(() => import('./FormPage.vue'))
 const ListPage = defineAsyncComponent(() => import('./ListPage.vue'))
 
 
-
+const app = useAppStore()
 const store = useMutasiStore()
 const route = useRoute()
 const title = computed(() => route?.meta?.title)
-
-onMounted(() => {
-  // console.log('Mounted ', title.value);
-  store.status = null
-  store.per_page = 20
-  Promise.all([
-    store.fetchAll(),
-    // console.log('store', store.items)
-  ])
+const loading = ref(false)
+const company = computed(() => {
+  return app?.form || null
 })
+const cabangList = ref([])
+// onMounted(() => {
+//   // console.log('Mounted ', title.value);
+//   store.status = null
+//   store.per_page = 20
+//   store.tujuan = ''
+//   store.dari = ''
+//   Promise.all([
+//     store.fetchAll(),
+//     // console.log('store', store.items)
+//   ])
+// })
+onMounted(async () => {
+  store.items = []
+  await app.fetchData()
+  await loadCabang()
+})
+async function loadCabang() {
+  loading.value = true
+  try {
+    const response = await api.get('/api/v1/transactions/mutasi/get-cabang')
+    if (response.status === 200) {
+      // const allcabang = response.data
+      // cabangList.value = allcabang.data.filter(a => a.kodecabang === kodetoko.value).map(c => c.kodecabang)
+      // console.log('cabangList', cabangList.value);
+      // await nextTick()
+      // const payload = {
+      //     from: getYearStartDate(),
+      //     to: getYearEndDate(),
+      //     dari: cabangList.value,
+      //     status: '2'
+      // }
+      // Promise.all([
+      const allcabang = response.data?.data
+      const mainCabang = allcabang.find(
+        c => c.kodecabang === company.value?.kode_toko
+      )
+
+      if (!mainCabang) {
+        cabangList.value = []
+        return
+      }
+      const targetUrl = mainCabang.url
+      cabangList.value = allcabang.filter(
+        c => c.url === targetUrl
+      ).map(c => c.kodecabang)
+      // const allcabang = response.data
+      // cabangList.value = allcabang.data.filter(a => a.kodecabang === kodetoko.value).map(c => c.kodecabang)
+      console.log('cabangList', cabangList.value);
+      await nextTick()
+      store.range.start_date = getYearStartDate()
+      store.range.end_date = getYearEndDate()
+      store.status = null
+      store.dari = cabangList.value
+      store.tujuan = ''
+      await store.fetchAll()
+    }
+  } catch (err) {
+    console.error('Gagal load cabang:', err)
+    err.message || 'Gagal memuat data cabang'
+  } finally {
+    loading.value = false
+  }
+}
 
 </script>
