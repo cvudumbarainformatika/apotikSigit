7af4a0393ce910cee929b80fbbe410427d54b4dc
duro06 <andi.meka.025@gmail.com>
2025-12-31 23:53:08 +0700

    rev : stok list

--- src/views/transaksi/stock/IndexPage.vue
+++ src/views/transaksi/stock/IndexPage.vue
@@ -6,32 +6,76 @@ import { inject } from 'vue'
 const $confirm = inject('confirm')
 import BaseMaster from '@/components/templates/BaseMaster.vue'
 import LoaderItem from './LoaderItem.vue'
+import { useAppStore } from '@/stores/app'
 const ListComp = defineAsyncComponent(() => import('./ListComp.vue'))
 const CetakData = defineAsyncComponent(() => import('./CetakData.vue'))
 
 const store = useStockStore()
 const route = useRoute()
 const title = computed(() => route.meta.title)
+const app = useAppStore()
+const loading = ref(false)
 
 onMounted(() => {
   console.log('Mounted ', title.value);
 
   store.per_page = 100
-  Promise.all([
+  Promise.all([    
+    handleRange(),
     store.fetchAll(),
     // console.log('Refresh List', store.items)
   ])
 })
 
+
+const handleRange = async () => {
+  // console.log('handleRange', store.range);
+  loading.value = true
+  const params = {
+    bulan: store.range?.start_date,
+    tahun: store.range?.end_date,
+    q: store.q,
+    page: store.page,
+    per_page: store.per_page,
+    depo: store.depo
+  }
+  console.log('params', params);
+  store.loading = true
+  store.items = []
+  try {
+    store.fetchAll(params)
+    // const response = await api.get(`api/v1/transactions/stok/get-kartu-stok`, { params })
+    // if (response) {
+    //   store.items = response.data.data
+    //   console.log('items params', store.items)
+    // }
+  } catch (error) {
+    console.error('Error fetching Kartu Stok:', error)
+  } finally {
+    store.loading = false
+    loading.value = true
+  }
+  // console.log('items', store.items);
+}
+
 function handleRefresh() {
+  const params = {
+    bulan: store.range?.start_date,
+    tahun: store.range?.end_date,
+    q: store.q,
+    page: store.page,
+    per_page: store.per_page,
+    depo: store.depo
+  }
   // console.log('Refresh List')
-  store.fetchAll()
+  store.fetchAll(params)
   // console.log('Refr', store.items)
 }
+
 </script>
 
 <template>
-  <base-master :title="title" :store="store" :showPrintButton="true" :showAddButton="false"
+  <base-master :title="title" :store="store" :showPrintButton="true" :showAddButton="false" :showCabangButton="true" :onCabang="handleRange"
     :onRefresh="handleRefresh">
     <template #loading>
       <LoaderItem />
